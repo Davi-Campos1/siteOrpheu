3cd17976c2ead44c6b9bd8e71947811967d192dd
Davi-Campos1 <camposdavi.010@gmail.com>
2026-03-05 19:39:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-// Rotas das páginas
+// Rotas das páginas existentes
 app.get('/', (req, res) => res.render('index'));
 app.get('/produto', (req, res) => res.render('produto'));
 app.get('/fotos', (req, res) => res.render('fotos'));
@@ -15,7 +15,25 @@ app.get('/detalhes', (req, res) => res.render('detalhes'));
 app.get('/detalhes2', (req, res) => res.render('detalhes2'));
 app.get('/detalhes3', (req, res) => res.render('detalhes3'));
 
-// Rota POST - Salvando dados de forma ASSÍNCRONA (Desafio)
+// --- NOVA ROTA: Lendo e exibindo os dados (Requisitos A e B) ---
+app.get('/registros', (req, res) => {
+    const caminhoArquivo = path.join(__dirname, 'data', 'contatos.json');
+
+    fs.readFile(caminhoArquivo, 'utf8', (err, data) => {
+        let lista = [];
+        if (!err && data) {
+            try {
+                lista = JSON.parse(data); // Converte para vetor de objetos
+            } catch (e) {
+                lista = [];
+            }
+        }
+        // Renderiza a nova página passando o vetor (Requisito B)
+        res.render('exibirDados', { contatos: lista });
+    });
+});
+
+// Sua rota POST atual (já está certinha!)
 app.post('/enviar', (req, res) => {
     const novoContato = req.body;
     const caminhoArquivo = path.join(__dirname, 'data', 'contatos.json');
@@ -23,10 +41,8 @@ app.post('/enviar', (req, res) => {
     fs.readFile(caminhoArquivo, 'utf8', (err, data) => {
         let lista = [];
         if (!err && data) { lista = JSON.parse(data); }
-        
         lista.push(novoContato);
 
-        // writeFile não trava o servidor enquanto grava (Melhor performance!)
         fs.writeFile(caminhoArquivo, JSON.stringify(lista, null, 2), (err) => {
             if (err) return res.send("Erro no servidor");
             res.render('sucesso', { nome: novoContato.nomeCompleto });
